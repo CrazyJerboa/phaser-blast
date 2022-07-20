ca6d7ac6a3bdd50d12b029d7d2a049ea6429d331
alexeygubov <alexey.gubov@gmail.com>
2022-07-20 14:56:22 +0300

    prototype: logic: scoring, turns counting and game over logic

--- scenes/Boot.scene.js
+++ scenes/Boot.scene.js
@@ -5,6 +5,9 @@ const COLS = 6;
 const RECT_SIZE = 50;
 const COLORS = [0xff0000, 0x00ff00, 0x0000ff, 0xffff00, 0x00ffff, 0xff00ff];
 
+const TURNS = 10;
+const SCORES_TO_WIN = 40;
+
 let lastId = 1;
 
 export default class BootScene extends Phaser.Scene {
@@ -20,6 +23,8 @@ export default class BootScene extends Phaser.Scene {
 
     create() {
         this.generateSheet(ROWS, COLS);
+        
+        this.addText();
     }
 
     update() {
@@ -34,7 +39,42 @@ export default class BootScene extends Phaser.Scene {
     }
 
     onSheetChanged() {
+        console.log('!!!')
+        if (this.scores >= SCORES_TO_WIN) {
+            this.gameOver('youWon');
+        } else {
+            if (this.turns <= 0) {
+                this.gameOver('noMoreTurns');
+            }
+        }
+    }
+    
+    addText() {
+        const textOffset = 20;
+        const screenWidth = this.game.scale.gameSize.width;
         
+        this.turns = TURNS;
+        this.scores = 0;
+        
+        this.scoresToWinText = this.add
+            .text(0, textOffset, 'Score ' + SCORES_TO_WIN + ' points to win!');
+        
+        this.turnsText = this.add
+            .text(0, textOffset * 3, 'Turns: ' + this.turns);
+        this.scoresText = this.add
+            .text(0, textOffset * 4, 'Scores: ' + this.scores);
+        
+        this.scoresToWinText.x = screenWidth - this.scoresToWinText.width - textOffset;
+        this.turnsText.x = screenWidth - this.turnsText.width - textOffset;
+        this.scoresText.x = screenWidth - this.scoresText.width - textOffset;
+    }
+    
+    updateTurnText() {
+        this.turnsText.setText('Turns: ' + this.turns);
+    }
+    
+    updateScoresText() {
+        this.scoresText.setText('Scores: ' + this.scores);
     }
     
     getTileNeighbours(clickedTile, depth = 0) {
@@ -92,6 +132,9 @@ export default class BootScene extends Phaser.Scene {
     
     destroySelectedTiles() {
         const deletedTiles = {};
+
+        this.scores += this.selectedTilesList.length;
+        this.updateScoresText();
 
         this.selectedTilesList.forEach(id => {
             const tile = this.children.getByName('rect_' + id);
@@ -178,6 +221,9 @@ export default class BootScene extends Phaser.Scene {
         
         const ths = this;
         tile.on('pointerup', function() {
+            ths.turns--;
+            ths.updateTurnText();
+            
             ths.selectedTilesList = [];
             ths.getTileNeighbours(tile);
 
@@ -241,9 +287,36 @@ export default class BootScene extends Phaser.Scene {
         if (!canTurn) {
             this.gameOver('noTurnsAvailable')
         }
+        
+        this.onSheetChanged();
     }
 
     gameOver(reason) {
+        const screenWidth = this.game.scale.gameSize.width;
+        const screenHeight = this.game.scale.gameSize.height;
+        
         console.log(reason)
+        switch (reason) {
+            case 'noMoreTurns':
+                this.add.text(screenWidth / 2, screenHeight / 2, 'Game Over');
+                this.add.text(screenWidth / 2, screenHeight / 2 + 20, 'You have run out of turns', {align: 'center'});
+                break;
+                
+            case 'noTurnsAvailable':
+                this.add.text(screenWidth / 2, screenHeight / 2, 'Game Over');
+                this.add.text(screenWidth / 2, screenHeight / 2 + 20, 'No any turns available', {align: 'center'});
+                break;
+                
+            case 'youWon':
+                this.add.text(screenWidth / 2, screenHeight / 2, 'You Won!');
+                this.add.text(screenWidth / 2, screenHeight / 2 + 20, 'Collected scores: ' + this.scores, {align: 'center'});
+                break;
+                
+            default:
+                this.add.text(screenWidth / 2, screenHeight / 2, 'Game Over');
+                break;
+        }
+        
+        this.scene.pause();
     }
 }
