afef61a8413031fa7037b3cd00da7c442ceee127
alexeygubov <alexey.gubov@gmail.com>
2022-07-23 17:22:54 +0300

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     entry: './main.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'build/dist'),
+        path: path.resolve(__dirname, 'dist'),
     },
     module: {
         rules: [
@@ -46,7 +46,7 @@ module.exports = {
             patterns: [
                 {
                     from: path.resolve(__dirname, './assets'),
-                    to: path.resolve(__dirname, './build/dist/assets'),
+                    to: path.resolve(__dirname, './dist/assets'),
                 }
             ]
         }),
